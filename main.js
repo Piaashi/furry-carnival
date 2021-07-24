a52c39bfa8b5c6e51dc5c4ab8ea45f8c3df4e478
Piaashi <75792228+Piaashi@users.noreply.github.com>
2021-07-24 14:33:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Check1()
+{
+    document.getElementById("A1").innerHTML="fan,umbrella,";
+    document.getElementById("M1").innerHTML="art, colours"
+}
+
+
+function Check2()
+{
+    document.getElementById("A2").innerHTML="TV,tablet,robot,screen";
+    document.getElementById("M2").innerHTML="computer,laptop,keyboard"
+}
+
+
+function Check3()
+{
+    document.getElementById("A2").innerHTML="girl,flowers,basket";
+    document.getElementById("M2").innerHTML="toy,doll,flowers"
+}
